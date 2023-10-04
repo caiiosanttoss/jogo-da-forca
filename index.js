6b26d9980e41ea209cadae848ce57d07559defbc
Augusto Oliveira <augustocesarfmo@gmail.com>
2023-10-04 16:27:37 -0300

    🎉 feat: add event click

--- index.js
+++ index.js
@@ -6,17 +6,24 @@ let tentativas = 6
 let numLetrasAcertadas = 0;
 const letrasReveladas = Array(palavraSplitted.length).fill("")
 
+function disableButton(id) {
+  document.getElementById(id).classList.add("disabled");
+}
+
 function encontrarLetra(letra) {
+  console.log(`Letra clicada: ${letra}`)
+  disableButton(letra);
+
   if (numLetrasAcertadas === palavraSplitted.length) {
-    console.log("Parabéns, você ganhou o jogo!")
+    // console.log("Parabéns, você ganhou o jogo!")
     return;
   }
   if (tentativas < 1) {
-    console.log("Você perdeu o jogo!")
+    // console.log("Você perdeu o jogo!")
     return;
   }
 
-  console.log(`Letra pressionada: ${letra}`)
+  // console.log(`Letra pressionada: ${letra}`)
   let encontrada = false;
 
   for (let index = 0; index < palavraSplitted.length; index++) {
@@ -33,40 +40,19 @@ function encontrarLetra(letra) {
     tentativas--;
   }
   
-  console.log(`Tentativas: ${tentativas}`)
-  console.log(letrasReveladas);
+  // console.log(`Tentativas: ${tentativas}`)
+  // console.log(letrasReveladas);
 }
-
-// 1º tentativa
-encontrarLetra("a");
-
-// 2º tentativa
-encontrarLetra("m");
-
-// 3º tentativa
-encontrarLetra("o");
-
-// 4º tentativa
-encontrarLetra("r");
-
-// 5º tentativa
-encontrarLetra("r");
-
-// 5º tentativa
-encontrarLetra("r");
-
-// 5º tentativa
-encontrarLetra("r");
 
 /**
  * Generates a virtual keyboard from A-Z in HTML.
  */
 function generateKeyboard() {
-  const mainElement = document.getElementById("main");
+  const mainElement = document.querySelector(".keyboard");
 
   for (let index = 65; index < 91; index++) {
     const letter = String.fromCharCode(index)
-    const buttonElement = `<button type="button" class="btn btn-primary">${letter}</button>`
+    const buttonElement = `<button id="${letter}" onclick="encontrarLetra('${letter}')" type="button" class="btn btn-primary">${letter}</button>`
 
     mainElement.innerHTML += buttonElement;
   }
